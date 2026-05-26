d4111723a1cca8a6a60ac37feeb78f65b498d078
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-27 00:10:26 +0100

    feat: implement tf-atom-db-subnet-group-aws module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
-output "enabled" {
-  description = "Whether the module is enabled."
-  value       = local.enabled
+output "id" {
+  description = "ID of the DB subnet group"
+  value       = try(aws_db_subnet_group.this[0].id, null)
+}
+
+output "name" {
+  description = "Name of the DB subnet group"
+  value       = try(aws_db_subnet_group.this[0].name, null)
+}
+
+output "arn" {
+  description = "ARN of the DB subnet group"
+  value       = try(aws_db_subnet_group.this[0].arn, null)
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,14 @@
-# -----------------------------------------------------------------------------
-# Module-Specific Variables
-#
-# Note: Standard labeling variables (enabled, namespace, tenant, environment,
-# stage, name, delimiter, attributes, tags, label_order, etc.) are provided
-# by context.tf via the tf-label module.
-# -----------------------------------------------------------------------------
+variable "subnet_ids" {
+  description = "List of subnet IDs for the DB subnet group"
+  type        = list(string)
+  validation {
+    condition     = length(var.subnet_ids) >= 2
+    error_message = "At least 2 subnet IDs are required for a DB subnet group."
+  }
+}
+
+variable "description" {
+  description = "Description of the DB subnet group"
+  type        = string
+  default     = null
+}
